e2b732e9de33427d95a63a3561336ad88b270dda
Faustino Vasquez <vasquez.limon@gmail.com>
2025-10-30 13:46:13 -0700

    Modificaciones para que funcione con importaciones y exportaciones de tipo ES6

--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,26 @@
-const express = require('express');
-const path = require('path');
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
 
-const startServer = (options) => {
-    const { port, public_path = 'public' } = options;
-    const app = express();
 
-    //para poder usar middelwares se usa el metodo use
-    app.use(express.static(path.join(__dirname, '../..', public_path))); //contenido estatido en public_path
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-    app.get('/', (req, res) => {
-        const indexPath = path.join(__dirname, '../..', public_path, 'index.html');
-        res.sendFile(indexPath);
-    });
 
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
+export const startServer = (options) => {
+  const { port, public_path = 'public' } = options;
+  const app = express();
+
+  //para poder usar middelwares se usa el metodo use
+  app.use(express.static(path.join(__dirname, '../..', public_path))); //contenido estatido en public_path
+
+  app.get(/.*/, (req, res) => {
+    const indexPath = path.join(__dirname, '../..', public_path, 'index.html');
+    res.sendFile(indexPath);
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }
 
-module.exports = { startServer };
